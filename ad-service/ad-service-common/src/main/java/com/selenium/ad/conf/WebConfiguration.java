5627c884a28566634fcab15f5181517d3f12b3ff
wantao666 <895484122@qq.com>
2020-09-27 22:13:29 +0800

    统一配置管理

---
+++ ad-service/ad-service-common/src/main/java/com/selenium/ad/conf/WebConfiguration.java
@@ -0,0 +1,20 @@
+package com.selenium.ad.conf;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+/**
+ * 统一配置管理
+ */
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.clear();
+        converters.add(new MappingJackson2HttpMessageConverter());
+    }
+}
